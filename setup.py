b910497f03848e075909d71fbdb1a944f4edf2e7
Hugo Camboulive <hugo@camboulive.me>
2016-12-12 19:02:40 +0100

    Add python setup script with dependencies

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='LeBonPrix',
+    version=0.1,
+    install_requires = [
+        'requests',
+        'numpy',
+        'scipy',
+        'scikit-learn'  
+    ]
+)
