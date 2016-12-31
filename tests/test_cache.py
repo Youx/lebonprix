22bbe8db302565353a6dfcd1b11168c6c5565359
Hugo Camboulive <hugo@camboulive.me>
2016-12-31 17:30:55 +0100

    Backend: add tests

---
+++ tests/test_cache.py
@@ -0,0 +1,54 @@
+import unittest, unittest.mock
+from lebonprix.cache import Cache
+
+
+class TestCache(unittest.TestCase):
+    def setUp(self):
+        self.subject = Cache()
+        self.item = {
+            'list_id': 1234,
+            'other_data': {
+                'qwe': 'qwe'
+            }
+        }
+
+    def test_init_creates_client(self):
+        with unittest.mock.patch('lebonprix.cache.pymongo') as pymongo:
+            c = Cache()
+            pymongo.MongoClient.assert_called_once_with()
+
+    def test_init_creates_index_on_items_list_id(self):
+        with unittest.mock.patch('lebonprix.cache.pymongo') as pymongo:
+            c = Cache()
+            pymongo.MongoClient().lebonprix.items.create_index.assert_called_once_with('list_id')
+    
+    def test_get_finds_one_item_by_list_id(self):
+        with unittest.mock.patch('lebonprix.cache.pymongo') as pymongo:
+            c = Cache()
+            c.get(1234)
+            pymongo.MongoClient().lebonprix.items.find_one.assert_called_once_with(
+                {'list_id': 1234}
+            )
+    
+    def test_get_returns_mongo_data(self):
+        with unittest.mock.patch('lebonprix.cache.pymongo') as pymongo:
+            c = Cache()
+            pymongo.MongoClient().lebonprix.items.find_one.return_value = self.item
+            res = c.get(1234)
+            self.assertEqual(res, self.item)
+
+    def test_set_upserts_item_with_list_id(self):
+        item = {
+            'list_id': 1234,
+            'other_data': {
+                'qwe': 'qwe'
+            }
+        }
+        with unittest.mock.patch('lebonprix.cache.pymongo') as pymongo:
+            c = Cache()
+            c.set(self.item)
+            pymongo.MongoClient().lebonprix.items.update.assert_called_once_with(
+                {'list_id': 1234},
+                self.item,
+                upsert=True
+            )
--- tests/test_lr.py
+++ tests/test_lr.py
@@ -7,19 +7,20 @@ import numpy.testing as npt
 class TestLinearRegression(unittest.TestCase):
     def setUp(self):
         self.subject = LinearRegression()
+        self.x = np.array([[2012, 24000, 1, 0],
+		                   [2013, 50000, 0, 0],
+		                   [2012, 38000, 0, 0],
+		                   [2012, 31000, 1, 1]])
+        self.y = np.array([21000,
+                           21900,
+                           22000,
+                           22440])
 
-    def test_linear_regression(self):
-        x = np.array([[2012, 24000, 1, 0],
-		              [2013, 50000, 0, 0],
-		              [2012, 38000, 0, 0],
-		              [2012, 31000, 1, 1]])
-        y = np.array([21000,
-                      21900,
-                      22000,
-                      22440])
+    def test_linear_regression_returns_correct_guess(self):
         guess = np.array([[2012, 24000, 1, 0]])
-        print(self.subject.lr(x, y, x))
+        res = self.subject.lr(self.x, self.y, guess)
+        npt.assert_array_equal(res, [21000])
 
-
-if __name__  == '__main__':
-    unittest.main()
+    def test_linear_regression_returns_array_of_results(self):
+        res = self.subject.lr(self.x, self.y, self.x)
+        npt.assert_array_equal(res, [21000, 21900, 22000, 22440])
